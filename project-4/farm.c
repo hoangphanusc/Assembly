028304892aa48a894347258f9d92aec49ea62782
vmagrobot <v+gitbot@vinnie.io>
2017-10-08 23:18:59 -0700

    [course staff] project-4

---
+++ project-4/farm.c
@@ -0,0 +1,83 @@
+/* This function marks the start of the farm */
+int start_farm()
+{
+    return 1;
+}
+
+unsigned getval_263()
+{
+    return 2421715793U;
+}
+
+unsigned getval_240()
+{
+    return 3281016873U;
+}
+
+unsigned addval_472(unsigned x)
+{
+    return x + 3347663019U;
+}
+
+unsigned getval_111()
+{
+    return 3284633928U;
+}
+
+/* This function marks the middle of the farm */
+int mid_farm()
+{
+    return 1;
+}
+
+/* Add two arguments */
+long add_xy(long x, long y)
+{
+    return x+y;
+}
+
+unsigned getval_405()
+{
+    return 2430634312U;
+}
+
+unsigned addval_384(unsigned x)
+{
+    return x + 3286272328U;
+}
+
+unsigned addval_436(unsigned x)
+{
+    return x + 3380920969U;
+}
+
+void setval_146(unsigned *p)
+{
+    *p = 3380920969U;
+}
+
+unsigned addval_213(unsigned x)
+{
+    return x + 1573114249U;
+}
+
+void setval_224(unsigned *p)
+{
+    *p = 3224949129U;
+}
+
+unsigned addval_482(unsigned x)
+{
+    return x + 3229929097U;
+}
+
+unsigned addval_114(unsigned x)
+{
+    return x + 2425409161U;
+}
+
+/* This function marks the end of the farm */
+int end_farm()
+{
+    return 1;
+}
